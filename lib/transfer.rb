40216a0a89115b4d735d54b2b2ef1e0590b7993d
Lauren Days <lmchambers0420@gmail.com>
2019-04-23 00:56:00 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
   end 
   
   def execute_transaction
-    if valid? && sender.balance > amount && self.status = 
+    if valid? && sender.balance > amount && self.status == "pending"
   end 
   
   
