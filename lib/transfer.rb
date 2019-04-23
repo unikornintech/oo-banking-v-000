1992f2d96e830713835cdb064770884dc2e949ae
Lauren Days <lmchambers0420@gmail.com>
2019-04-23 00:39:18 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,12 +2,13 @@ require 'pry'
 
 class Transfer
   attr_reader :sender, :receiver
-  attr_accessor :status
+  attr_accessor :status, :amount
   
   def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
     @status = "pending"
+    @amount = amount
   end 
   
   
